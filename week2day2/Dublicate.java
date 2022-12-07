980029e5cfe09bf2a6dcabd4d988c66cbf03bba5
MuthuGW <119443671+MuthuGW@users.noreply.github.com>
2022-12-07 11:40:32 +0530

    Add files via upload

---
+++ week2day2/Dublicate.java
@@ -0,0 +1,20 @@
+package week2day2;
+
+public class Dublicate
+{
+	public static void main(String[] args) {
+		   int[] num= {11,12,11,21,88,21,88};
+		   for(int i=0;i < num.length;i++)
+		   {
+			   for(int j=i+1;j< num.length;j++)
+			   {
+				   if(num[i]==num[j])
+				   {
+					   System.out.println(num[i]);
+				   }
+			   }
+				   
+			   }
+		   }
+
+}
